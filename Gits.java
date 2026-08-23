a51c7896d7198935d0138a64f69cf44eafd07fac
Leonardo Machado <leo.md@outlook.com>
2026-08-22 23:48:35 -0300

    metodos corrigos + metodo b

--- Gits.java
+++ Gits.java
@@ -7,15 +7,24 @@ public class Gits {
         this.l = new ArrayList<>();
     }
 
-    public int nroOcorrencias (ArrayList<Integer> l , Integer el){
-        int cont=0;
-        for(int i = 0 ; i < l.size();i++){
-            if (l.get(i).equals(el)){
+    public int nroOcorrencias(Integer el) {
+        int cont = 0;
+        for (int i = 0; i < this.l.size(); i++) {
+            if (this.l.get(i).equals(el)) {
                 cont++;
             }
         }
-        return cont++; 
+        return cont; 
     }
 
-    
+    public boolean hasRepeat() {
+        for (int i = 0; i < this.l.size(); i++) {
+            for (int j = i + 1; j < this.l.size(); j++) {
+                if (this.l.get(i).equals(this.l.get(j))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
