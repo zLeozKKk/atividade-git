1fb3a67cde904bdba35cd29eb4041fdd3e464afc
Leonardo Machado <leo.md@outlook.com>
2026-08-28 18:27:31 -0300

    feat: adiciona estrutura base e metodos

--- Gits.java
+++ Gits.java
@@ -1,21 +1,38 @@
-import java.util.ArrayList; 
+import java.util.ArrayList;
 
 public class Gits {
-    ArrayList<Integer> l; 
 
+    private ArrayList<Integer> l;
+
+   
     public Gits() {
         this.l = new ArrayList<>();
     }
 
-    public int nroOcorrencias(Integer el) {
+    
+    public int nOcorrencias(Integer el) {
+        if (this.l == null || el == null) return 0;
         int cont = 0;
         for (int i = 0; i < this.l.size(); i++) {
             if (this.l.get(i).equals(el)) {
                 cont++;
             }
         }
-        return cont; 
+        return cont;
     }
+
     
-    
+    public boolean hasRepeat() {
+        if (this.l == null) return false;
+        for (int i = 0; i < this.l.size(); i++) {
+            for (int j = i + 1; j < this.l.size(); j++) {
+                if (this.l.get(i).equals(this.l.get(j))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+  
 }
